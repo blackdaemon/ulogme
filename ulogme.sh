30eda42d663f83a1d3b36ffc34ab13e7241a41fb
Pavel Vitis "blackdaemon" <pavelvitis@gmail.com>
2016-09-28 23:05:15 +0200

    Allow to run ulogme from anywhere

--- ulogme.sh
+++ ulogme.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # vim:set ff=unix tabstop=4 shiftwidth=4 expandtab:
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
 if [ "$(uname)" == "Darwin" ]; then
   # This is a Mac
   ./osx/run_ulogme_osx.sh
