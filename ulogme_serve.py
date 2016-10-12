7f75a099a877790910d0c2a1763968ad3a0179bd
Pavel Vitis "blackdaemon" <pavelvitis@gmail.com>
2016-10-12 22:46:31 +0200

    Do not fail web server on data parsing errors

--- ulogme_serve.py
+++ ulogme_serve.py
@@ -38,8 +38,14 @@ def get_status_output(cmd):
 class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         # default behavior
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self) 
-
+        try:
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self) 
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            self.wfile.write(e)
+            
     def do_POST(self):
         form = cgi.FieldStorage(
             fp = self.rfile,
@@ -49,37 +55,52 @@ class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         result = 'NOT_UNDERSTOOD'
 
         if self.path == '/refresh':
-            # recompute jsons. We have to pop out to root from render directory
-            # temporarily. It's a little ugly
-            refresh_time = form.getvalue('time')
-            os.chdir(rootdir) # pop out
-            updateEvents() # defined in export_events.py
-            os.chdir('render') # pop back to render directory
-            result = 'OK'
+            try:
+                # recompute jsons. We have to pop out to root from render directory
+                # temporarily. It's a little ugly
+                refresh_time = form.getvalue('time')
+                os.chdir(rootdir) # pop out
+                updateEvents() # defined in export_events.py
+            except Exception as e:
+                # TODO: Should we make the errors visible?
+                logging.error(e)
+            finally:
+                os.chdir('render') # go back to render
+                result = 'OK'
       
         if self.path == '/addnote':
-            # add note at specified time and refresh
-            note = form.getvalue('note')
-            note_time = form.getvalue('time')
-            os.chdir(rootdir) # pop out
-            cmd_status, cmd_stdout = get_status_output('echo %s | ./note.sh %s' % (note, note_time))
-            updateEvents() # defined in export_events.py
-            os.chdir('render') # go back to render
-            result = 'OK'
+            try:
+                # add note at specified time and refresh
+                note = form.getvalue('note')
+                note_time = form.getvalue('time')
+                os.chdir(rootdir) # pop out
+                cmd_status, cmd_stdout = get_status_output('echo %s | ./note.sh %s' % (note, note_time))
+                updateEvents() # defined in export_events.py
+            except Exception as e:
+                # TODO: Should we make the errors visible?
+                logging.error(e)
+            finally:
+                os.chdir('render') # go back to render
+                result = 'OK'
 
         if self.path == '/blog':
-            # add note at specified time and refresh
-            post = form.getvalue('post')
-            if post is None:
-                post = ''
-            post_time = int(form.getvalue('time'))
-            os.chdir(rootdir) # pop out
-            trev = rewindTime(post_time)
-            with open('logs/blog_%d.txt' % (post_time, ), 'w') as bf:
-                bf.write(post)
-            updateEvents() # defined in export_events.py
-            os.chdir('render') # go back to render
-            result = 'OK'
+            try:
+                # add note at specified time and refresh
+                post = form.getvalue('post')
+                if post is None:
+                    post = ''
+                post_time = int(form.getvalue('time'))
+                os.chdir(rootdir) # pop out
+                trev = rewindTime(post_time)
+                with open('logs/blog_%d.txt' % (post_time, ), 'w') as bf:
+                    bf.write(post)
+                updateEvents() # defined in export_events.py
+            except Exception as e:
+                # TODO: Should we make the errors visible?
+                logging.error(e)
+            finally:
+                os.chdir('render') # go back to render
+                result = 'OK'
     
         self.send_response(200)
         self.send_header('Content-type','text/html')
