12f51331b22c5addc7f45229692a2a56427e9bbe
Pavel Vitis "blackdaemon" <pavelvitis@gmail.com>
2016-10-18 00:19:39 +0200

    Add set/getCookie functions

--- render/ulogme_common.js
+++ render/ulogme_common.js
@@ -1,24 +1,6 @@
 // Common utility functions
 // TODO: Expand this out, make everything more modular and cleaner
 
-function setCookie(c_name, value, exdays) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
-}
-
-function getCookie(c_name) {
-    var i, x, y, ARRcookies = document.cookie.split(";");
-    for (i = 0; i < ARRcookies.length; i++) {
-        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-        x = x.replace(/^\s+|\s+$/g, "");
-        if (x == c_name) {
-            return unescape(y);
-        }
-    }
-}
 
 // builds a dictionary of string -> color, in HSL format
 function colorHashStrings(arr, s, l) {
