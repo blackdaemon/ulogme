082df4cb4f15ceaa4a6f36220ba2746f0715867e
Pavel Vitis "blackdaemon" <pavelvitis@gmail.com>
2016-10-22 03:19:06 +0200

    Initial version

--- render/render_settings_example.js
+++ render/render_settings_example.js
@@ -56,13 +56,6 @@ display_groups.push(["Matlab", "SubText2 Coding", "INotebook", "Terminal", "Pape
 display_groups.push(["TeXworks"]); // paper writing related
 display_groups.push(["Computer locked", "Computer idle", "Computer suspended"]); // computer not being used 
 
-// Activity groups to group related work. This will be shown as inner piechart ring.
-// All related activities will be shown in outer piechart ring.
-var activity_groups = [];
-activity_groups.push({name:"Work",titles:["Matlab", "INotebook", "Papers", "Terminal", "SubText2", "SubText2 Coding", "Latex"]});
-activity_groups.push({name:"Other",titles:["Google Chrome", "Gmail", "MISC"]});
-activity_groups.push({name:"Idle",titles:["Computer locked", "Computer idle", "Computer suspended"]});
-
 // list of titles that classify as "hacking", or being productive in general
 // the main goal of the day is to get a lot of focused sessions of hacking
 // done throughout the day. Windows that arent in this list do not
@@ -83,3 +76,121 @@ var draw_coffee = false;
 
 // Reload interval in minutes. Set to 0 to turn off.
 var auto_reload_interval = 5;
+
+var show_productivity_pulse = true;
+var show_productivity_gain = true;
+var skip_zero_weekend_pulse = true;
+const PRODUCTIVITY_PULSE_NAME = "Productivity score";
+
+// Productivity levels and their weights (in percentage)
+const PL_VERY_PRODUCTIVE = 100;	// If you worked for 4 hours of 8 on this, your score would be 50%
+const PL_PRODUCTIVE = 75;
+const PL_NEUTRAL = 50;	// If you worked for 4 hours of 8 on this, your score would be 25%
+const PL_DISTRACTED = 25;
+const PL_INACTIVE = 0;
+
+const PL_NAMES = {};
+PL_NAMES[PL_VERY_PRODUCTIVE] =  "Very productive";
+PL_NAMES[PL_PRODUCTIVE] = "Productive";
+PL_NAMES[PL_NEUTRAL] = "Distracted";
+PL_NAMES[PL_DISTRACTED] = "Very distracted";
+PL_NAMES[PL_INACTIVE] = "Inactive";
+
+// These groups will be rendered together in the "barcode view". For example, I like
+// to group my work stuff and play stuff together.
+var display_groups = [];
+display_groups.push(["GVIM", "gedit", "Terminal", "Mail", "RTC", "IDA", "Sametime chats", "Sametime group chats", "SQL", "DataStage", "Web browser (work)", "File browser", "LibreOffice"]); // work related
+display_groups.push(["OneNote notes", "Todoist", "Claiming", "Expense Reimbursement", "ulogme"]); // administrative tasks
+display_groups.push(["Web browser (other)", "Gmail", "YouTube", "Mplayer", "Video", "anubis (rdesktop)", "Eclipse IDE (ulogme)", "Eclipse IDE (enso)", "MISC"]); // others
+display_groups.push(["Computer locked", "Computer idle", "Computer suspended"]); // computer not being used 
+
+// Activity groups to group related work. This will be shown as inner piechart ring.
+// All related activities will be in outer piechart ring.
+var activity_groups = [];
+activity_groups.push({
+	name: "Work",
+	color: 'hsl(356, 66%, 48%)',
+	plevel: PL_VERY_PRODUCTIVE,
+	titles:[
+	        "Terminal", "Mail", "RTC", "IDA", 
+	        "Sametime chats", "Sametime group chats", "SQL", 
+	        "DataStage", "Web browser (work)", "File browser", 
+	        "LibreOffice", "OneNote notes", "Todoist", "Claiming", 
+	        "Expense Reimbursement",
+	        "Meetings",
+	]
+});
+activity_groups.push({
+	name: "Learning",
+	color: 'hsl(14, 81%, 59%)',
+	plevel: PL_PRODUCTIVE,
+	titles:[
+	        "Learning",
+	]
+});
+activity_groups.push({
+	name: "Other",
+	color: 'hsl(185, 100%, 35%)',
+	plevel: PL_NEUTRAL,
+	titles:[
+	        "Web browser (other)",  
+	        "Eclipse IDE", 
+	        "ulogme",
+	        "GVIM", "gedit", 
+	]
+});
+activity_groups.push({
+	name: "Distracted",
+	color: 'hsl(46, 81%, 62%)',
+	plevel: PL_DISTRACTED,
+	titles:[
+	        "Gmail", "YouTube", "Mplayer", 
+	        "Video", "anubis (rdesktop)", 
+	        "Eclipse IDE (ulogme)", "Eclipse IDE (enso)", 
+	        "MISC"
+	]
+});
+activity_groups.push({
+	name: "Idle",
+	color: 'hsl(18, 27%, 24%)',
+	plevel: PL_INACTIVE,
+	titles:[
+	        "Computer locked", "Computer idle", "Computer suspended"
+	]
+});
+
+// Represent time of day as object
+// Example:
+//   t = time("15:30"); t.getHours(); t.getMinutes(); t.getSeconds(); t.toString(); 
+function time(time_str) {
+	var _time = new Date(
+		"1970-01-01 {hr:min}:00".replace('{hr:min}', time_str)
+	);
+
+	return {
+		getHours: function() {
+			return _time.getHours();
+		},
+		getMinutes: function() {
+			return _time.getMinutes();
+		},
+		getSeconds: function() {
+			return _time.getSeconds();
+		},
+		getTime: function() {
+			return _time.getTime();
+		},
+		toString: function() {
+			return _time.toLocalTimeString(); // or use `toTimeString()`
+		}
+	};
+}
+
+var working_hours = [];
+working_hours.push({ name: "Sunday", start: null, end: null });
+working_hours.push({ name: "Monday", start: time("09:00"), end: time("17:00") });
+working_hours.push({ name: "Tuesday", start: time("09:00"), end: time("17:00") });
+working_hours.push({ name: "Wednesday", start: time("09:00"), end: time("17:00") });
+working_hours.push({ name: "Thursday", start: time("09:00"), end: time("17:00") });
+working_hours.push({ name: "Friday", start: time("09:00"), end: time("17:00") });
+working_hours.push({ name: "Saturday", start: null, end: null });
